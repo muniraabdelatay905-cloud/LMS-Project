fda67b793197e6b44741ea48254deb043110879c
muniraabdelatay905-cloud <muniraabdelatay905@gmail.com>
2026-06-26 12:38:45 +0300

    Add files via upload

--- scriptLms.js
+++ scriptLms.js
@@ -1,4 +1,13 @@
-const form=document.getElementById("courseForm");
-form.addEventListener("submit",function(event))
-
-
+let form=document.getElementById("courseForm");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let courseName=document.getElementById("courseName").Value;
+    let instructor=document.getElementById("instructorName").value;
+    if(courseName===""||instructor===""){
+        document.getElementById("message").innerText="Please fill all fields.";
+        document.getElementById("message").style.color="red";
+    }else{
+        document.getElementById("message").innerText=
+        courseName+"added successfully!";
+        document.getElementById("message").style.color="green";
+    }});
